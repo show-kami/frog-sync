4ffa1e070deff755c8eb7c582aafc8e5f50a07ec
Show.KAMI from SourceTree <mhasegawa.net@gmail.com>
2016-11-29 20:02:04 +0900

    【改良】Rでの描画の高速化
    高水準作図関数plotを使い続けることをやめ，一度描画した後は低水準作図関数pointsを使うことにした。

--- drawPhaseAnimation.R
+++ drawPhaseAnimation.R
@@ -2,13 +2,12 @@ library(animation)
 library(jpeg)
 library(tcltk)
 
-num.frogs <- 2 # 考えている個体数
+num.frogs <- 100 # 考えている個体数
 data <- read.csv("output_theta.csv", header = T)
 num.timeslice <- nrow(data)
-
 theta <- seq(0, 2*pi, by=0.01)
 
-# pb <- txtProgressBar(min = 1, max = num.timeslice, style = 3) # プログレスバーの設定
+pb <- txtProgressBar(min = 1, max = num.timeslice, style = 3) # プログレスバーの設定
 # プログレスバーについて http://gg-hogehoge.hatenablog.com/entry/2013/08/18/095448
 
 execute <- function(){
@@ -18,12 +17,10 @@ execute <- function(){
 		plot(cos(theta), sin(theta), type = "l", xlim = c(-1,1), ylim = c(-1,1) , xlab = "", ylab = "")
 		for(j in 1:num.frogs){
 			phase_j <- data[i, 1+j]
-			par(new = T)
-			plot(cos(phase_j), sin(phase_j) , pch = 16, cex = 3, col = "black", xlim = c(-1,1), ylim = c(-1,1) , xlab = "", ylab = "")
+			points(cos(phase_j), sin(phase_j) , pch = 16, cex = 3, col = "black")
 		}
-		par(new = T)
-		plot(OrderParaX, OrderParaY, pch = 16, cex = 3, col = "gold", xlim = c(-1,1), ylim = c(-1,1))
-		# setTxtProgressBar(pb, i) 
+		points(OrderParaX, OrderParaY, pch = 16, cex = 3, col = "gold")
+		setTxtProgressBar(pb, i) 
 	}
 }
 
--- drawSpaceDistribution.R
+++ drawSpaceDistribution.R
@@ -8,19 +8,17 @@ pb <- txtProgressBar(min = 1, max = num.timeslice, style = 3)
 
 execute <- function(){
 	for(i in 1:num.timeslice){
-		plot(0,0, pch = 16, col = "white", xlim = c(0,10), ylim = c(0,10))
+		plot(0,0,type = "n", xlim = c(0,10), ylim = c(0,10))
 
 		for(x in 1:10){
 			for(y in 1:10){
-				par(new = T)
 				pos = (x-1) + 10 * (y-1)
-				if(data[i, pos+2] == -1){
-					plot(x-1,y-1, pch = 16, cex = 3, col = "black", xlim = c(0,10), ylim = c(0,10))
-				} else {
-					plot(x-1,y-1, pch = 16, cex = 3, col = "red", xlim = c(0,10), ylim = c(0,10))
+				if(data[i, pos+2] != -1){
+					points(x-1,y-1, pch = 16, cex = 3, col = "red")
 				}
 			}
 		}
+
 		setTxtProgressBar(pb, i) 
 	}
 }
